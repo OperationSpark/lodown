e7ff933165914114e9ea0338ea5fb2c15893d7ac
John Fraboni <johnfraboni@hotmail.com>
2016-01-24 07:09:41 +0000

    * add config

---
+++ .master/README.md
@@ -0,0 +1,4 @@
+# lodown
+
+See the README.md file in the root directory. This is just a stub README to 
+avoid warning messages.
--- .master/pr.sh
+++ .master/pr.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 set -ev
+
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-	cd .master && npm install && wget https://raw.github.com/jfraboni/spec/master/ld/.master/test/i.spec.js -P test/ && npm test
+    cd .master 
+    config=()
+
+    getConfig() {
+        i=0
+        while read line 
+        do
+            config[i]=$line
+            i=$(($i + 1))
+        done < $1
+    }
+    getConfig "c.txt"
+    echo "Running pull request tests for spec ${config[0]}..."
+	npm install && wget config[0] -P test/ && npm test
 fi
