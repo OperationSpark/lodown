7280681ee858bdc6cc748d2776bf808f64d84c5e
John Fraboni <johnfraboni@hotmail.com>
2016-01-24 06:12:14 +0000

    * update wget

--- .master/pr.sh
+++ .master/pr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-	cd .master && npm install && wget https://raw.github.com/jfraboni/os/master/ld/i.spec.js && npm test
+	cd .master && npm install && wget https://raw.github.com/jfraboni/os/master/ld/i.spec.js -P test/ && npm test
 fi
