d5e9560553bcf14f51ee273ecb8ce1c774218b96
John Fraboni <johnfraboni@hotmail.com>
2016-01-24 07:20:27 +0000

    * update pr

--- .master/pr.sh
+++ .master/pr.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -ev
 
+config=()
+
+getConfig() {
+    i=0
+    while read line 
+    do
+        config[i]=$line
+        echo config[i]
+        i=$(($i + 1))
+    done < $1
+}
+
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     cd .master
-    ls
-    config=()
-
-    getConfig() {
-        i=0
-        while read line 
-        do
-            config[i]=$line
-            echo config[i]
-            i=$(($i + 1))
-        done < $1
-    }
     getConfig "c.txt"
     echo "Running pull request tests for spec ${config[0]}..."
 	npm install && wget ${config[0]} -P test/ && npm test
