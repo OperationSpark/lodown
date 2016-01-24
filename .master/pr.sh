4f63f10fcc2729df3908d419ee37b5b2b0e89063
John Fraboni <johnfraboni@hotmail.com>
2016-01-24 06:58:50 +0000

    * remove cruft

--- .master/pr.sh
+++ .master/pr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-	cd .master && npm install && wget https://raw.github.com/jfraboni/os/master/ld/i.spec.js -P test/ && npm test
+	cd .master && npm install && wget https://raw.github.com/jfraboni/spec/master/ld/.master/test/i.spec.js -P test/ && npm test
 fi
