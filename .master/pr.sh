64b519e4bca72002cee94289a89c2f96ff27649a
John Fraboni <johnfraboni@hotmail.com>
2016-01-24 17:43:14 +0000

    * fix pr if

--- .master/pr.sh
+++ .master/pr.sh
@@ -3,7 +3,7 @@ set -ev
 
 echo "pull request is ${TRAVIS_PULL_REQUEST}."
 
-if [ "${TRAVIS_PULL_REQUEST}" -ne "false" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
     cd .master
     readarray a < c.txt
     echo "Running pull request tests for spec ${a[0]}..."
