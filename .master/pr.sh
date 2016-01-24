8bdc6060076e74afe201f0aa8ca951a3d6873fe6
John Fraboni <johnfraboni@hotmail.com>
2016-01-24 07:36:35 +0000

    * update pr

--- .master/pr.sh
+++ .master/pr.sh
@@ -3,7 +3,7 @@ set -ev
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     cd .master
-    IFS=$'\n' read -d '' -r -a config < c.txt
-    echo "Running pull request tests for spec ${config[0]}..."
-	npm install && wget "${config[0]}" -P test/ && npm test
+    readarray a < c.txt
+    echo "Running pull request tests for spec ${a[0]}..."
+	npm install && wget "${a[0]}" -P test/ && npm test
 fi
