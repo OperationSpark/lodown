e02f22410a26cca9a10907be3c14d9d98f6c6edd
benspector3 <benj.spector@gmail.com>
2019-03-28 11:18:12 -0500

    modified template of index.js to optionally allow use in non Node environments

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
-'use strict';
-
-// YOU KNOW WHAT TO DO //
-
-/**
- * each: Designed to loop over a collection, Array or Object, and applies the 
- * action Function to each value in the collection.
- * 
- * @param {Array or Object} collection: The collection over which to iterate.
- * @param {Function} action: The Function to be applied to each value in the 
- * collection
- */
-function each(collection, action) {
-    if(Array.isArray(collection)) {
-        for(var i = 0; i < collection.length; i++) {
-            action(collection[i], i, collection);
+(function(root) {
+    'use strict';
+    
+    const _ = {};
+    
+    // YOU KNOW WHAT TO DO //
+    
+    /**
+     * each: Designed to loop over a collection, Array or Object, and applies the 
+     * action Function to each value in the collection.
+     * 
+     * @param {Array or Object} collection: The collection over which to iterate.
+     * @param {Function} action: The Function to be applied to each value in the 
+     * collection
+     * 
+     * Usage:
+     * 
+     *      const arr = [1, 2, 3, 4, 5];
+     *      function multiplyBy5(item, loc, collection) {
+     *          console.log(item * 5);
+     *      }
+     *      each(arr, multiplyeBy5);    //-> 5, 10, 15, 20, 25
+     */
+    _.each = function(collection, action) {
+        if (Array.isArray(collection)) {
+            for (var i = 0; i < collection.length; i++) {
+                action(collection[i], i, collection);
+            }
         }
-    } else {
-        for (var key in collection) {
-            action(collection[key], key, collection);
+        else {
+            for (var key in collection) {
+                action(collection[key], key, collection);
+            }
         }
     }
-}
-module.exports.each = each;
+    
+    
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+    
+    
+    //////////////////////////////////////////////////////////////////////////
+    ///////// DO NOT DELETE //////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+    if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+        module.exports = _;
+    }
+    else if (root) {
+        root._ = _;
+    }
+}(this));
